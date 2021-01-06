9c070b1ee06e43bf17dba2cb2897a1a115d3cf17
Petri Savola <petri.savola@supercell.com>
2021-01-06 23:18:51 +0200

    Remove global image caching to avoid OOM. Add localized caching later for track selection.

--- server/src/main/java/gp/ImageCache.java
+++ server/src/main/java/gp/ImageCache.java
@@ -16,40 +16,21 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public abstract class ImageCache {
-    private static Map<String, BufferedImage> imageCache = new ConcurrentHashMap<>();
 
     public static BufferedImage getImage(String name) {
-        BufferedImage image = imageCache.get(name);
-        if (image != null) {
-            return image;
-        }
         try (InputStream is = ImageCache.class.getResourceAsStream(name)) {
-            image = ImageIO.read(is);
+            return toCompatibleImage(ImageIO.read(is));
         } catch (IOException e) {
             throw new RuntimeException("Image " + name + " is missing");
         }
-        return updateCache(image, name, imageCache);
     }
 
     public static BufferedImage getImageFromPath(String absolutePath) {
-        BufferedImage image = imageCache.get(absolutePath);
-        if (image != null) {
-            return image;
-        }
         try (InputStream is = new FileInputStream(absolutePath)) {
-            image = ImageIO.read(is);
+            return toCompatibleImage(ImageIO.read(is));
         } catch (IOException e) {
             throw new RuntimeException("Image " + absolutePath + " is missing");
         }
-        return updateCache(image, absolutePath, imageCache);
-    }
-
-    private static <T> BufferedImage updateCache(BufferedImage image, T key, Map<T, BufferedImage> cache) {
-        image = toCompatibleImage(image);
-        cache.put(key, image);
-        //DataBuffer buff = image.getRaster().getDataBuffer();
-        //System.err.println("Bytes cached " + key + ": " + buff.getSize() * DataBuffer.getDataTypeSize(buff.getDataType()) / 8);
-        return image;
     }
 
     private static BufferedImage toCompatibleImage(BufferedImage image) {
